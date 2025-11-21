3bd5e8d42f03610eedc9dbe117b20f7ca0118741
Vishnu SK <svsathieshvishnu194@gmail.com>
2025-11-21 21:50:48 +0530

    Add files via upload

---
+++ adv.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import numpy as numpy
+import joblib 
+import sremlit as st
+
+#load the ml model 
+
+model = joblib.load(open("linear_regression_model.joblib",'rb'))
+st.title("Sale Prediction App")
+
+#Input feature 
+TV = st.number_input("TV ADV budget",min_value = 0.0)
+Radio = st.number_input("Radio Adv budget",min_value=0.0)
+Newspaper = st.number_input("Newspaper Adv budget",min_value=0.0)
+
+#Make Pred 
+
+if st.button('Predict Sales'):
+    input_data = np.array([[TV, Radio ,Newspaper]])
+    prediction = model.predict(input_data)[0]
+    st.sucess(f'Predict Sales:{prediction:.2f}')
+    
